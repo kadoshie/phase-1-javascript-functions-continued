e13dacc8916c4677b49e1fff562303c363bc7ac3
Moringa School <ruthmutiso69@gmail.com>
2022-09-23 09:55:24 +0300

    Done with the assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function saturdayFun(activity="roller-skate") {
+
+    console.log(`This Saturday, I want to ${activity}!`)
+    return `This Saturday, I want to ${activity}!`
+}
+// SaturdayFun()
+
+function mondayWork(doWhat = 'go to the office'){
+
+    console.log(`This Monday, I Will ${doWhat}.`);
+    return `This Monday, I will ${doWhat}.`
+}
+
+function wrapAdjective(par1="*") {
+    // const encouragingPromptFunction = wrapAdjective("!!!")
+    return function(emphatic="special"){
+        return `You are ${par1}${emphatic}${par1}!`
+    }
+}
